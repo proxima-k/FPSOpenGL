b3918f13d0828fdfa63224adb7c2dedc7310e8cc
GoodGuyDeg <Robin.zeitlin@hotmail.com>
2024-10-27 12:06:50 +0100

    WIP : Improve basic enemy

--- src/game/enemies/CubeEnemy.cpp
+++ src/game/enemies/CubeEnemy.cpp
@@ -18,38 +18,43 @@ CubeEnemy::CubeEnemy(glm::vec3 position, MeshRenderer meshRenderer)
 
 	collision_channel = Collision_Channel::Enemy;
 
+    enemyType = chooseVariant();
+
 	xpAmount = 10.0f;
 	maxHealth = 10.0f;
 
 	health = maxHealth;
 }
 
-
 void CubeEnemy::update(float deltaTime)
 {
-	glm::vec3 camPos = Camera::mainCamera->transform.position;
-	glm::vec3 dir = glm::normalize(camPos - transform.position);
-	 
-	glm::vec3 speed = glm::vec3(20);
+    glm::vec3 camPos = Camera::mainCamera->transform.position;
+    glm::vec3 dirToCamera = glm::normalize(camPos - transform.position);
 
-	physicsBody.acceleration = glm::vec3(0.0f);
-	physicsBody.acceleration += dir * speed;
+    glm::vec3 speed = glm::vec3(20);
+    glm::vec3 forwardDir = -transform.getForward();
+    physicsBody.acceleration = glm::vec3(0.0f);
+    physicsBody.acceleration += forwardDir * speed;
 
-	transform.position += physicsBody.velocity * deltaTime;
-	transform.rotation = glm::quatLookAt(glm::normalize(dir), glm::vec3(0, 1, 0));
+    transform.position += physicsBody.velocity * deltaTime;
 
-	if (glm::distance(lastPosition, transform.position) > 0.05f) 
-	{
-		TrailMesh* trailMesh = trailRenderer.getMesh();
+    glm::quat targetRotation = glm::quatLookAt(dirToCamera, glm::vec3(0, 1, 0));
+    float rotationSpeed = 3.0f;
+    transform.rotation = glm::slerp(transform.rotation, targetRotation, rotationSpeed * deltaTime);
 
-		glm::vec3 newPos = transform.position;
+    if (glm::distance(lastPosition, transform.position) > 0.05f)
+    {
+        TrailMesh* trailMesh = trailRenderer.getMesh();
+        glm::vec3 newPos = transform.position;
+        trailMesh->addVertex(newPos, 0.1f);
+        lastPosition = transform.position;
+    }
 
-		trailMesh->addVertex(glm::vec3(newPos), .1f);
-		
-		lastPosition = transform.position;
-	}
+    physicsBody.update();
+    trailRenderer.draw(transform.position, transform.rotation, glm::vec3(1.0f));
+}
 
-	physicsBody.update();
-
-	trailRenderer.draw(transform.position, transform.rotation, glm::vec3(1.0f));
+CubeEnemy::Variants CubeEnemy::chooseVariant()
+{
+    return Variants::Basic;
 }
--- src/game/enemies/CubeEnemy.h
+++ src/game/enemies/CubeEnemy.h
@@ -8,9 +8,20 @@
 class CubeEnemy : public Enemy 
 {
 public:
+	enum Variants
+	{
+		Basic,
+		Cosine,
+		Sine,
+	};
+
+
 	CubeEnemy(glm::vec3 position, MeshRenderer meshRenderer);
 
 	void update(float deltaTime) override;
+
+	Variants chooseVariant();
+	Variants enemyType;
 
 	Physicsbody physicsBody;
 
