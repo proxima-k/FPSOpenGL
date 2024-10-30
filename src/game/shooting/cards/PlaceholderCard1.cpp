8af23ef011c048b5a776315b4b6839c364b3a52c
AndyPandyAgent <andyl2070@gmail.com>
2024-10-30 16:14:29 +0100

    Shotgun card

--- src/game/shooting/cards/PlaceholderCard1.cpp
+++ src/game/shooting/cards/PlaceholderCard1.cpp
@@ -1,15 +1,16 @@
 #include "PlaceHolderCard1.h"
+#include "DefaultCard.h"
+#include "../Shooter.h"
+#include "../../Player.h"
 
 void PlaceHolderCard1::update(float deltaTime)
 {
-	timeElapsed += deltaTime * 5;
+
 	glm::vec3 rightVector = glm::cross(launchDirection, upDirection);
 
-	height = glm::sin(timeElapsed) / 6;
-	currentPosition += launchDirection * deltaTime * 3.f;
+	currentPosition += launchDirection * deltaTime * 10.f;
 
 	transform.position = currentPosition + rightVector * height;
-	transform.rotation = glm::angleAxis(glm::radians(timeElapsed * 250), -transform.getUp());
 
 	Entity* hit_actor = game->get_coliding_entity(this, Collision_Channel::Enemy);
 	if (hit_actor != nullptr)
@@ -23,12 +24,34 @@ void PlaceHolderCard1::update(float deltaTime)
 				destroy();
 		}
 	}
+
+	timer.updateTimer(deltaTime);
 }
 
 void PlaceHolderCard1::launch(glm::vec3 launchPosition, glm::vec3 launchDirection, glm::vec3 upDirection)
 {
 	bDestroyOnHit = false;
 	damage = 10;
+
+	if (bCanSpawnChildren)
+	{
+		glm::vec3 rightVector = glm::cross(launchDirection, upDirection);
+
+		float spreadAngle = glm::radians(10.0f);
+
+		/*PlaceHolderCard1* leftCard;
+		game->player->shooter.spawnCard(leftCard, transform.position, launchDirection + spreadAngle, upDirection);
+		leftCard->bCanSpawnChildren = false;
+		leftCard->launch(launchPosition, launchDirection + spreadAngle, upDirection);
+
+		PlaceHolderCard1* rightCard;
+		game->player->shooter.spawnCard(rightCard, transform.position, launchDirection + -spreadAngle, upDirection);
+		rightCard->bCanSpawnChildren = false;
+		rightCard->launch(launchPosition, launchDirection + -spreadAngle, upDirection);*/
+
+		game->player->shooter.shootDefault(transform.position, launchDirection + spreadAngle, upDirection);
+		game->player->shooter.shootDefault(transform.position, launchDirection - spreadAngle, upDirection);
+	}
 
 	Card::launch(launchPosition, launchDirection, upDirection);
 	currentPosition = launchPosition;
--- src/game/shooting/cards/PlaceholderCard1.h
+++ src/game/shooting/cards/PlaceholderCard1.h
@@ -7,19 +7,25 @@ class PlaceHolderCard1 : public Card
 {
 public:
 	PlaceHolderCard1(glm::vec3 position, MeshRenderer meshRenderer)
-		: Card(position, meshRenderer) {}
+		: Card(position, meshRenderer), bCanSpawnChildren(true) {}
 
 	void update(float deltaTime) override;
 	void launch(glm::vec3 launchPosition, glm::vec3 launchDirection, glm::vec3 upDirection) override;
 
 	glm::vec3 getMeshColor() const override { return glm::vec3(0.3f, 1.f, 0.3f); }
-
 	CardType getCardType() const override { return Placeholder1; }
+
+	Mesh* cardMesh;
+	Shader* cardShader;
+	Camera* camera;
 
 private:
 	float height = 0.f;
 	float timeElapsed = 0.f;
-
 	glm::vec3 currentPosition;
+
+	bool bCanSpawnChildren;
+
+
 };
 
