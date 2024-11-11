9bf509b28467e03b203b430e52d8f564247fcbf4
RobinZeitlin <144109907+RobinZeitlin@users.noreply.github.com>
2024-11-11 10:20:51 +0100

    WIP : Better visualise the spawning of enemies.

--- src/game/Enemy.cpp
+++ src/game/Enemy.cpp
@@ -8,4 +8,10 @@ Enemy::Enemy(glm::vec3 position)
 
     transform.scale = glm::vec3(.2f);
     transform.position = position;
+
+    // make this more visually clear
+    // maybe some elastic bouncing curves would be good
+    // or maybe some type of crazy particle explosion?
+    // audio play?
+    game->spawn_particle_source(transform.position, 60.0f, 0.6f, 2.0f);
 }
