29145ac8b642e76675d6fc91003011b7977d8c79
proxima-k <leonnkent@gmail.com>
2024-11-28 12:44:11 +0100

    fix: boss launch pillars now won't go out of cage bounds

--- src/game/enemies/boss/LaunchPillarTask.cpp
+++ src/game/enemies/boss/LaunchPillarTask.cpp
@@ -6,6 +6,7 @@
 #include "BossEnemy.h"
 #include "BossCage.h"
 
+
 void LaunchPillarTask::onNodeStart(BT::Blackboard& blackboard)
 {
     currentAttackCount = 0;
@@ -27,12 +28,22 @@ BT::NodeState LaunchPillarTask::onNodeUpdate(float deltaTime, BT::Blackboard& bl
 		Player* player = blackboard.getValue<Player*>("player");
 		if (player == nullptr)
 			return BT::NodeState::FAILURE;
-        if (floorGrid == nullptr)
-			return BT::NodeState::FAILURE;
 
+        BossCage* cage = boss->bossCage;
+        Grid* floorGrid = cage->floorGrid;
         float constant = 3.5f;
-        glm::vec2 gridCellCenter = boss->bossCage->floorGrid->getCellCenter(player->transform.position + glm::normalize(player->physicsbody.velocity) * constant);
-        glm::vec3 spawnPosition = glm::vec3(gridCellCenter.x, 0, gridCellCenter.y);
+        glm::vec3 predictPosition = player->transform.position + glm::normalize(player->physicsbody.velocity) * constant;
+
+        glm::vec2 cellIndex = floorGrid->getCellCoords(predictPosition - cage->centerWorldPosition);
+        std::cout << cellIndex.x << "   " << cellIndex.y << std::endl;
+        
+        int xCellCount = cage->xCellCount;
+        int zCellCount = cage->zCellCount;
+        cellIndex.x = glm::clamp((int)cellIndex.x, -xCellCount, xCellCount - 1);
+        cellIndex.y = glm::clamp((int)cellIndex.y, -zCellCount, zCellCount - 1);
+
+        glm::vec2 gridCellCenter = floorGrid->getCellCenter(cellIndex.x, cellIndex.y);
+        glm::vec3 spawnPosition = glm::vec3(gridCellCenter.x, 0, gridCellCenter.y) + cage->centerWorldPosition;
 		spawnPosition.y = 0;
         game->spawn_entity<PillarEnemy>(spawnPosition);
 
